00ed3d6e07331f444cc020f93b7310cac61e84b1
Bryan Richter <b@chreekat.net>
2013-12-25 13:54:02 -0800

    Clean up things unused by BaroboLab

--- html-gen.hs
+++ html-gen.hs
@@ -16,14 +16,9 @@ index = do
     docTypeHtml ! lang "en" $ do
         H.head $ do
             meta ! charset "utf-8"
-            meta ! httpEquiv "X-UA-Compatible" ! content "IE=edge"
             H.title "BaroboLab - DEMO"
-            meta ! name "description" ! content ""
-            meta ! name "viewport" ! content "width=device-width, initial-scale=1"
-            --  Place favicon.ico and apple-touch-icon.png in the root directory 
             link ! rel "stylesheet" ! href "css/bootstrap.min.css"
             link ! rel "stylesheet" ! href "css/main.css"
-            script ! src "js/vendor/modernizr-2.6.2.min.js" $ mempty
         body $ do
             header $ a ! href "index.html" $ img ! src "img/barobo_logo.png"
             nav $ ol ! class_ "nav nav-stacked nav-pills" $ li ! class_ "active" $ a ! href "index.html" $ "BaroboLab"
